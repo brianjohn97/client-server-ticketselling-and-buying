428aa08a9cf74c1adf1d0470339a44e041df121d
brianjohn97 <johnson.brian997@gmail.com>
2023-05-14 13:59:10 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# client-server-ticketselling-and-buying
+ 
---
+++ server.c
@@ -0,0 +1,51 @@
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <errno.h>
+#include <string.h>
+#include <sys/types.h>
+#include <time.h> 
+#include <math.h>
+
+#define PI 4.0*atan(1.0)
+#define BUFFER_SIZE 1024
+#define PORT_NUMBER 5437
+
+int main(int argc, char *argv[])
+{
+    int listenfd = 0, connfd = 0;
+    struct sockaddr_in serv_addr; 
+
+    char sendBuff[BUFFER_SIZE];
+
+    listenfd = socket(AF_INET, SOCK_STREAM, 0);
+    memset(&serv_addr, '0', sizeof(serv_addr));
+    memset(sendBuff, '0', sizeof(sendBuff)); 
+
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_addr.s_addr = htonl(INADDR_ANY);
+    serv_addr.sin_port = htons(PORT_NUMBER); 
+
+    bind(listenfd, (struct sockaddr*)&serv_addr, sizeof(serv_addr)); 
+
+    listen(listenfd, 10); 
+
+    while(1)
+    {
+        connfd = accept(listenfd, (struct sockaddr*)NULL, NULL); 
+
+	time_t t = time(NULL);
+	struct tm  * tm = localtime (&t);
+	char s[64];
+	strftime(s,sizeof(s),"%c",tm);
+        sprintf(sendBuff,"Message received from the server: %s\t%d\t%f\n",s,121,PI);
+	
+	write(connfd, sendBuff, strlen(sendBuff)); 
+
+        close(connfd);
+        sleep(1);
+     }
+}
